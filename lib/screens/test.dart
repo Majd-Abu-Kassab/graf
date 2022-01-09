610e2203fb08682db9e566a0ab374264e433690e
Majd <majdabokassab@yahoo.com>
2022-01-09 19:18:16 +0200

    appbar 2.3

--- lib/screens/test.dart
+++ lib/screens/test.dart
@@ -60,3 +60,74 @@
 //     }
 //   }
 // }
+
+
+
+
+
+
+
+
+
+// @dart=2.9
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      home: new MyTabbedPage(),
+    );
+  }
+}
+
+class MyTabbedPage extends StatefulWidget {
+  const MyTabbedPage({ Key key}) : super(key: key);
+
+  @override
+  _MyTabbedPageState createState() => new _MyTabbedPageState();
+}
+
+class _MyTabbedPageState extends State<MyTabbedPage> with SingleTickerProviderStateMixin {
+  final List<Tab> myTabs = <Tab>[
+    new Tab(text: 'LEFT'),
+    new Tab(text: 'RIGHT'),
+  ];
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(vsync: this, length: myTabs.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Tab demo"),
+        bottom: new TabBar(
+          controller: _tabController,
+          tabs: myTabs,
+        ),
+      ),
+      body: new TabBarView(
+        controller: _tabController,
+        children: myTabs.map((Tab tab) {
+          return new Center(child: new Text(tab.text));
+        }).toList(),
+      ),
+
+    );
+  }
+}
