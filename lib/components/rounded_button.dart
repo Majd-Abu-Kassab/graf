0902e0c3a014845fdab073aeb04cd75a7721af8a
naseem <naseemhasasneh2000@gmail.com>
2022-01-10 21:36:53 +0200

    changes

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -11,7 +11,7 @@ class RoundedButton extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 16.0),
       child: Material(
-        elevation: 5.0,
+        elevation: 6.0,
         color: colour,
         borderRadius: BorderRadius.circular(30.0),
         child: MaterialButton(
